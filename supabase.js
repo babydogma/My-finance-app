68cc4f656e626ded4f668928829cd16df77e931a
babydogma <123qqwes@mail.ru>
2026-04-06 16:58:57 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://npjrrstyotancvjdflvl.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_mntROxzfSwKTEK1VUsLGSw_3PxzAJw-";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
