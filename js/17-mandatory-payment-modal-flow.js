6a887e9210af4ae0b0b0e65d43cb5ec5e366b5b1
babydogma <123qqwes@mail.ru>
2026-04-28 17:33:32 +0300

    Update 17-mandatory-payment-modal-flow.js

--- js/17-mandatory-payment-modal-flow.js
+++ js/17-mandatory-payment-modal-flow.js
@@ -34,80 +34,20 @@
     renderMonthStrip,
     renderModal,
   }) {
-    let savedPageScrollY = 0;
-    let isPageScrollLocked = false;
-
-    function lockPageScroll() {
-      if (isPageScrollLocked) return;
-
-      savedPageScrollY =
-        window.scrollY ||
-        document.documentElement.scrollTop ||
-        document.body.scrollTop ||
-        0;
-
-      isPageScrollLocked = true;
-
-      document.documentElement.classList.add("modal-scroll-locked");
-      document.body.classList.add("modal-scroll-locked");
-
-      document.documentElement.style.overflow = "hidden";
-      document.body.style.overflow = "hidden";
-      document.body.style.position = "fixed";
-      document.body.style.top = `-${savedPageScrollY}px`;
-      document.body.style.left = "0";
-      document.body.style.right = "0";
-      document.body.style.width = "100%";
-
-      document.documentElement.style.removeProperty("touch-action");
-      document.body.style.removeProperty("touch-action");
-    }
-
-    function unlockPageScroll() {
-      if (!isPageScrollLocked) {
-        forceUnlockPageScroll();
-        return;
-      }
-
-      const scrollY = savedPageScrollY;
-
-      isPageScrollLocked = false;
-      savedPageScrollY = 0;
-
-      forceUnlockPageScroll();
-
-      window.scrollTo(0, scrollY);
-    }
-
-    function forceUnlockPageScroll() {
-      document.documentElement.classList.remove("modal-scroll-locked");
-      document.body.classList.remove("modal-scroll-locked");
-
-      document.documentElement.style.removeProperty("overflow");
-      document.body.style.removeProperty("overflow");
-      document.body.style.removeProperty("position");
-      document.body.style.removeProperty("top");
-      document.body.style.removeProperty("left");
-      document.body.style.removeProperty("right");
-      document.body.style.removeProperty("width");
-      document.documentElement.style.removeProperty("touch-action");
-      document.body.style.removeProperty("touch-action");
-    }
-
-    function hardHideModal(modal) {
-      if (!modal) return;
-
-      modal.classList.add("hidden");
-      modal.classList.remove("is-visible");
-      modal.classList.remove("is-closing");
-    }
-
     function isModalOpen(modal) {
       return Boolean(
         modal &&
         !modal.classList.contains("hidden") &&
         !modal.classList.contains("is-closing")
       );
+    }
+
+    function closeModalIfOpen(modal) {
+      if (!isModalOpen(modal)) return;
+
+      closeAnimatedModal(modal, {
+        keepBodyLocked: false,
+      });
     }
 
     function resetMandatoryPaymentForm() {
@@ -139,29 +79,12 @@
     }
 
     function openMandatoryPaymentEditorModal() {
-      lockPageScroll();
       openAnimatedModal(mandatoryPaymentEditorModal);
     }
 
     function closeMandatoryPaymentEditorModal() {
-      closeAnimatedModal(mandatoryPaymentBucketPickerModal, {
-        keepBodyLocked: true,
-      });
-
-      closeAnimatedModal(mandatoryPaymentEditorModal, {
-        keepBodyLocked: true,
-      });
-
-      window.setTimeout(() => {
-        hardHideModal(mandatoryPaymentBucketPickerModal);
-        hardHideModal(mandatoryPaymentEditorModal);
-
-        if (isModalOpen(mandatoryPaymentsModal)) {
-          lockPageScroll();
-        } else {
-          unlockPageScroll();
-        }
-      }, 340);
+      closeModalIfOpen(mandatoryPaymentBucketPickerModal);
+      closeModalIfOpen(mandatoryPaymentEditorModal);
 
       resetMandatoryPaymentForm();
     }
@@ -224,25 +147,9 @@
     }
 
     function closeMandatoryPaymentsModal() {
-      closeAnimatedModal(mandatoryPaymentBucketPickerModal, {
-        keepBodyLocked: true,
-      });
-
-      closeAnimatedModal(mandatoryPaymentEditorModal, {
-        keepBodyLocked: true,
-      });
-
-      closeAnimatedModal(mandatoryPaymentsModal, {
-        keepBodyLocked: false,
-      });
-
-      window.setTimeout(() => {
-        hardHideModal(mandatoryPaymentBucketPickerModal);
-        hardHideModal(mandatoryPaymentEditorModal);
-        hardHideModal(mandatoryPaymentsModal);
-
-        unlockPageScroll();
-      }, 360);
+      closeModalIfOpen(mandatoryPaymentBucketPickerModal);
+      closeModalIfOpen(mandatoryPaymentEditorModal);
+      closeModalIfOpen(mandatoryPaymentsModal);
 
       resetMandatoryPaymentForm();
     }
@@ -253,7 +160,6 @@
       renderMonthStrip();
       renderModal();
 
-      lockPageScroll();
       openAnimatedModal(mandatoryPaymentsModal);
     }
 
